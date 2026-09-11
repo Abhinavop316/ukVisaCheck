e60d1df33e9c7f181d92d3fdcd7c1a6055e1f5fb
Abhinabop316 <embassyvisa001@gmail.com>
2026-09-12 02:59:09 +0530

    cure danger2

--- frontendUser/src/App.jsx
+++ frontendUser/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+// Guide Pages
+import EvisaOverviewPage from './pages/guide/EvisaOverviewPage';
+import WhatYouNeedPage from './pages/guide/WhatYouNeedPage';
+import SetUpUkviAccountPage from './pages/guide/SetUpUkviAccountPage';
+import UpdateUkviAccountPage from './pages/guide/UpdateUkviAccountPage';
+import ReportErrorPage from './pages/guide/ReportErrorPage';
+import IfCannotAccessPage from './pages/guide/IfCannotAccessPage';
+import TravelWithEvisaPage from './pages/guide/TravelWithEvisaPage';
+
 // Service Pages
 import ServiceStartPage from './pages/service/ServiceStartPage';
 import SignInDocumentPage from './pages/service/SignInDocumentPage';
@@ -16,10 +25,20 @@ import NotFoundPage from './pages/NotFoundPage';
 export default function App() {
   return (
     <Routes>
-      {/* Root redirect directly to Status check sign in */}
-      <Route path="/" element={<Navigate to="/status" replace />} />
+      {/* Home / Overview Page */}
+      <Route path="/" element={<EvisaOverviewPage />} />
 
-      {/* Interactive Service Routes (view-immigration-status.service.gov.uk) */}
+      {/* Guide Pages */}
+      <Route path="/evisa" element={<EvisaOverviewPage />} />
+      <Route path="/evisa/overview" element={<EvisaOverviewPage />} />
+      <Route path="/evisa/what-you-need-to-access-evisa" element={<WhatYouNeedPage />} />
+      <Route path="/evisa/set-up-ukvi-account" element={<SetUpUkviAccountPage />} />
+      <Route path="/evisa/update-ukvi-account" element={<UpdateUkviAccountPage />} />
+      <Route path="/evisa/report-error-evisa" element={<ReportErrorPage />} />
+      <Route path="/evisa/if-you-cannot-access-evisa" element={<IfCannotAccessPage />} />
+      <Route path="/evisa/travel-with-evisa" element={<TravelWithEvisaPage />} />
+
+      {/* Interactive Service Routes */}
       <Route path="/status" element={<SignInDocumentPage />} />
       <Route path="/status/view/:docNumber" element={<StatusProfilePage />} />
       <Route path="/service/start" element={<ServiceStartPage />} />
@@ -27,10 +46,6 @@ export default function App() {
       <Route path="/service/document-details" element={<DocumentDetailsPage />} />
       <Route path="/service/security-code" element={<SecurityCodePage />} />
       <Route path="/service/status-profile" element={<StatusProfilePage />} />
-
-      {/* Legacy and Guide redirects to /status */}
-      <Route path="/evisa/*" element={<Navigate to="/status" replace />} />
-      <Route path="/service/*" element={<Navigate to="/status" replace />} />
 
       {/* Application Status Checker */}
       <Route path="/check-application-status" element={<CheckStatusPage />} />
